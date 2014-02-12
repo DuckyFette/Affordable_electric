335603c5601fc9c94853144fca3cfee55ac7aa3d
Mike <troubledducky@.gmail.com>
2014-02-11 19:49:41 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AffordableElectric::Application.config.secret_key_base = '39a7d0a437e424a95991bb8dd2970a12b6c7ff2fb318746e1f0e3841cc1b786f75fac85a335ab5b0fac62cff2f83e344d32858956f804c4fe15025c37c5c95d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AffordableElectric::Application.config.session_store :cookie_store, key: '_Affordable_electric_session'
